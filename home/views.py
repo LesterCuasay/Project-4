385d7496ed3c950460b9621abbcd61fb2abe0c50
LesterCuasay <lestercuasay@live.co.uk>
2023-05-04 12:26:10 +0000

    Added url path to render html page

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse, request
+from django.views import generic
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'base.html')
