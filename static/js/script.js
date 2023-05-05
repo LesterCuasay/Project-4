7d8406e15ba9a227f604275a3113a635cefa3825
LesterCuasay <lestercuasay@live.co.uk>
2023-05-05 11:26:39 +0000

    script - added a script to get the current year for copyright purposes

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+function currentYear() {
+
+    let fullYear = new Date ();
+    let yearNow = fullYear.getFullYear();
+
+    document.getElementById("year").innerHTML = yearNow;
+}
+
+currentYear();
