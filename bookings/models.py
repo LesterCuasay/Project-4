b61c8cd290be6d32c93738b7ce3f0a11d5a85e3f
LesterCuasay <lestercuasay@live.co.uk>
2023-05-11 13:51:34 +0000

    fix - updated model.py for bookings

--- bookings/models.py
+++ bookings/models.py
@@ -4,11 +4,18 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class BookingForm(models.Model):
-    first_name = models.CharField(max_length=50, blank=True)
-    last_name = models.CharField(max_length=50, blank=True)
-    email = models.EmailField(max_length=100, blank=True)
+    name = models.CharField(
+        max_length=100, blank=True, verbose_name='Name', null=True)
+    email = models.EmailField(
+        default='example@example.com', verbose_name='Email')
     number_of_people = models.PositiveIntegerField(
         default=1,
-        validators=[MaxValueValidator(10), MinValueValidator(1)])
-    date = models.DateField()
-    special_requirements = models.TextField(blank=True)
+        validators=[MaxValueValidator(10), MinValueValidator(1)],
+        verbose_name='Number of People'
+    )
+    date = models.DateField(verbose_name='Date')
+    special_requirements = models.TextField(
+        blank=True, verbose_name='Special Requirements')
+
+    def __str__(self):
+        return self.name
