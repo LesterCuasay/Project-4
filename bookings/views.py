e37c306120ace8864fa7538edc87f1aa7f8d080d
LesterCuasay <lestercuasay@live.co.uk>
2023-05-17 11:30:58 +0000

    feat - updated view.py for booking form

--- bookings/views.py
+++ bookings/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.views import View
+from .models import BookingForm
+from .forms import BookingTableForm
 
 
-class Book(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'bookings/bookings.html')
-    
+def book_table(request):
+    booking_form = BookingTableForm()
+
+    if request.method == 'POST':
+        booking_form = BookingTableForm(request.POST)
+
+        if booking_form.is_valid():
+            booking_form.save()
+
+    context = {'form': booking_form}
+
+    return render(request, 'bookings/bookings.html', context)
