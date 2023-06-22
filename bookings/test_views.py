f8ca89aa6547bfacc3ade346dffd84cb6521d475
LesterCuasay <lestercuasay@live.co.uk>
2023-06-22 16:55:03 +0000

    feat - created a test for the book_table view

--- bookings/test_views.py
+++ bookings/test_views.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import BookingForm
 
+
+class BookingViewTest(TestCase):
+    """
+    Sets up a test user, test admin and a bookingform
+    so I can test the views using these parameters
+    """
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser",
+            password="testpassword"
+            )
+        self.admin = User.objects.create_superuser(
+            username="admin",
+            password="adminpassword"
+        )
+        self.booking = BookingForm.objects.create(
+            user=self.user,
+            date="2023-06-18",
+            time="12:00",
+            number_of_people=4
+        )
+
+    def test_book_table_view(self):
+        self.client.login(
+            username="testuser",
+            password="testpassword"
+        )
+        response = self.client.get(reverse('book_table'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response='bookings/bookings.html')
 
