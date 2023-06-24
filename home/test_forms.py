c78873e0da6bfecd797ac21804c042b819f8c5d1
LesterCuasay <lestercuasay@live.co.uk>
2023-06-24 17:56:46 +0000

    feat - deleted CustomLoginForm

--- home/test_forms.py
+++ home/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from home.forms import CustomLogInForm, CustomSignupForm, CustomResetForm, CustomResetKeyForm
+from home.forms import CustomSignupForm, CustomResetForm, CustomResetKeyForm
 
 
 class CustomFormsTest(TestCase):
