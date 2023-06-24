6c5ed55ca9d17de8d2fc31bef0a4781786406603
LesterCuasay <lestercuasay@live.co.uk>
2023-06-24 14:08:32 +0000

    feat - added a test to see if the menu view works as intended

--- home/test_views.py
+++ home/test_views.py
@@ -10,3 +10,11 @@ class HomeViewTest(TestCase):
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response='home/index.html')
+
+    def test_menu_view(self):
+        """
+        Tests the menu view
+        """
+        response = self.client.get(reverse('menu'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response='home/menu.html')
