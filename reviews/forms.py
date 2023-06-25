716d5ed8658961f1b4f663b72e6a950a73e4a25a
LesterCuasay <lestercuasay@live.co.uk>
2023-06-25 20:54:12 +0000

    feat - added a function on views to hide crispy forms labels

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,9 +1,16 @@
 from django import forms
+from django.forms.widgets import *
 from .models import BookingReview
+from crispy_forms.helper import FormHelper
 
 
 class BookingReviewForm(forms.ModelForm):
     class Meta:
         model = BookingReview
         fields = "__all__"
-        exclude = ["author"]
+        exclude = ["author", "status",]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_show_labels = False
