497c8fd71bcdfa65a7f57112fffd3e2c757f1ff4
LesterCuasay <lestercuasay@live.co.uk>
2023-06-25 11:25:19 +0000

    feat - created a basic view to post booking review

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from .models import BookingReview
+
+
+def create_review(request):
+    if request.method == "POST":
+        author = request.user
+        email = request.POST['email']
+        comment = request.POST['comment']
+
+    review = BookingReview(
+        author=author,
+        email=email,
+        comment=comment,
+    )
 
 
 def review_success(request):
