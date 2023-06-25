68f5545d1e2bf079153311a8bed443fbc66f568e
LesterCuasay <lestercuasay@live.co.uk>
2023-06-25 16:11:25 +0000

    feat - added the updated view into the index view

--- home/views.py
+++ home/views.py
@@ -3,8 +3,29 @@ from django.views import View
 
 
 class Index(View):
+    template_name = 'home/index.html'
+
     def get(self, request, *args, **kwargs):
-        return render(request, 'home/index.html')
+        review_form = BookingReviewForm()
+        context = {
+            "review_form": review_form,
+        }
+
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        review_form = BookingReviewForm(request.POST)
+        if request.method == "POST":
+            review = review_form.save(commit=False)
+            review.author = request.user
+            review.save()
+            return redirect('review_success')
+
+        context = {
+            "review_form": review_form,
+        }
+
+        return render(request, self.template_name, context)
 
 
 class Menu(View):
