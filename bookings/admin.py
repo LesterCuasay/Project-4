149ced299e9df6ea382039d5c4be2a2b2890ee37
LesterCuasay <lestercuasay@live.co.uk>
2023-06-25 10:34:25 +0000

    feat - added bookingformadmin to modify list display

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import BookingForm
 
+
+class BookingFormAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'email',
+        'phone_number',
+        'number_of_people',
+        'date',
+        'time',
+    )
+
+
 admin.site.register(BookingForm)
--- reviews/admin.py
+++ reviews/admin.py
@@ -3,7 +3,12 @@ from .models import BookingReview
 
 
 class BookingReviewAdmin(admin.ModelAdmin):
-    list_display = ('author', 'email', 'created_at', 'overall_rating')
+    list_display = (
+        'author',
+        'email',
+        'created_at',
+        'overall_rating'
+    )
 
 
 admin.site.register(BookingReview, BookingReviewAdmin)
