6f02d99215ab74db7f767ecbe0640b7e77a381bf
LesterCuasay <lestercuasay@live.co.uk>
2023-06-25 10:47:29 +0000

    feat - added a status field for BookingReview

--- reviews/models.py
+++ reviews/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+STATUS = ((0, "Draft"), (1, "Published"))
+
 
 class BookingReview(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -10,6 +12,7 @@ class BookingReview(models.Model):
     food_rating = models.IntegerField(default=0)
     overall_rating = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
 
     def __str__(self):
         return {self.author.username}
