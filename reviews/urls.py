a730ad996a5c765f96980bea3d36dd27ad2437f5
LesterCuasay <lestercuasay@live.co.uk>
2023-06-25 16:10:40 +0000

    fix - deleted create_review view as it was clashing with index url so will make the view inside Index view instead

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,11 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path(
-        "create",
-        views.create_review,
-        name="create_review",
-    ),
-    path(
         "success",
         views.review_success,
         name="review_success",
--- reviews/views.py
+++ reviews/views.py
@@ -1,26 +1,4 @@
 from django.shortcuts import render
-from .models import BookingReview
-from .forms import BookingReviewForm
-
-
-def create_review(request):
-    if request.method == "POST":
-        review_form = BookingReviewForm(request.POST)
-
-        if review_form.is_valid():
-            review = review_form.save(commit=False)
-            review.author = request.user
-            review.save()
-            return redirect('review_success')
-
-    else:
-        review_form = BookingReviewForm()
-
-    context = {
-        "review_form": review_form,
-    }
-
-    return render(request, "home/index.html", context)
 
 
 def review_success(request):
