d690eacd5470c1599a1020c821ed6fd8f93d6860
LesterCuasay <lestercuasay@live.co.uk>
2023-06-27 12:23:09 +0000

    feat - added view to see reviews

--- reviews/views.py
+++ reviews/views.py
@@ -9,8 +9,10 @@ class Index(View):
 
     def get(self, request, *args, **kwargs):
         review_form = BookingReviewForm()
+        reviews = BookingReview.objects.all()
         context = {
             "review_form": review_form,
+            "reviews": reviews
         }
 
         return render(request, self.template_name, context)
@@ -28,16 +30,6 @@ class Index(View):
         }
 
         return render(request, self.template_name, context)
-
-
-def review_view(request):
-    reviews = BookingReview.objects.all()
-
-    context = {
-        'reviews': reviews
-    }
-
-    return render(request, 'home/index.html', context)
 
 
 def review_success(request):
