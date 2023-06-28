46083801e73c6a6cada007fad1b18bfe175e19ca
LesterCuasay <lestercuasay@live.co.uk>
2023-06-28 14:18:15 +0000

    script - added a js function click on the starts and implement it in the django form

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,24 @@
-
 /** Function for copyright date  */
 
 function currentYear() {
 
-    let fullYear = new Date ();
+    let fullYear = new Date();
     let yearNow = fullYear.getFullYear();
 
     document.getElementById("year").innerHTML = yearNow;
 }
 
 currentYear();
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let starRatings = document.querySelectorAll('.rating-star')
+
+    starRatings.forEach(function (star) {
+        star.addEventListener('click', function () {
+            let starValue = this.getAttribute('data-value');
+            let fieldName = this.getAttribute('data-name');
+            document.querySelector('input[name="' + fieldName + '"]').value = starValue;
+        })
+    })
+})
