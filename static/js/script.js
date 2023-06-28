4786913f87108e00e18a99ba9eb13d534bdec7fa
LesterCuasay <lestercuasay@live.co.uk>
2023-06-28 14:28:10 +0000

    script - added a function for mouse-over add checked class

--- static/js/script.js
+++ static/js/script.js
@@ -14,11 +14,21 @@ currentYear();
 document.addEventListener('DOMContentLoaded', function () {
     let starRatings = document.querySelectorAll('.rating-star')
 
-    starRatings.forEach(function (star) {
+    starRatings.forEach(function(star) {
         star.addEventListener('click', function () {
             let starValue = this.getAttribute('data-value');
             let fieldName = this.getAttribute('data-name');
             document.querySelector('input[name="' + fieldName + '"]').value = starValue;
-        })
+        });
+
+        star.addEventListener('mouseover', function() {
+            let hoverStarValue = star.getAttribute('data-value');
+            let stars = Array.from(this.parentNode.children);
+
+            stars.forEach(function(star) {
+                let starValue = star.getAttribute('data-value');
+                star.classList.toggle('checked', starValue <= hoverStarValue);
+            });
+        });
     })
 })
