cadefb1a13aaa61672c992b53b7448bfd3ea72c4
LesterCuasay <lestercuasay@live.co.uk>
2023-06-28 10:04:37 +0000

    feat - added docstrings to views.py

--- reviews/views.py
+++ reviews/views.py
@@ -5,9 +5,16 @@ from .forms import BookingReviewForm
 
 
 class Index(View):
+    """
+    Renders homepage also showing the published reviews
+    to all users.
+    """
     template_name = 'home/index.html'
 
     def get(self, request, *args, **kwargs):
+        """
+        Shows the published reviews to all users.
+        """
         review_form = BookingReviewForm()
         reviews = (
             BookingReview.objects.filter(status=1).order_by('-created_at')
@@ -20,6 +27,11 @@ class Index(View):
         return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
+        """
+        Allows the user to submit a review, If the request method is POST
+        the form will be saved as draft review only an superuser can publish
+        the reviews. If the form is not valid it will render the index.html.
+        """
         review_form = BookingReviewForm(request.POST)
         if request.method == "POST":
             review = review_form.save(commit=False)
@@ -35,4 +47,7 @@ class Index(View):
 
 
 def review_success(request):
+    """
+    Renders review-sucess.html
+    """
     return render(request, 'reviews/review-success.html')
