2fc71ed22c1af7d90806e17ae2443ce87eefe88d
LesterCuasay <lestercuasay@live.co.uk>
2023-06-30 13:42:09 +0000

    feat - added a test for the review view

---
+++ reviews/tests/test_views.py
@@ -0,0 +1,43 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.core.exceptions import PermissionDenied
+from reviews.models import BookingReview
+
+
+class BookingReviewViewTest(TestCase):
+    """
+    Sets up a test use, test admin, and a reviewform
+    so I can test the views using these parameters
+    """
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser",
+            password="testpassword"
+            )
+        self.admin = User.objects.create_superuser(
+            username="admin",
+            password="adminpassword"
+        )
+        self.reviewform = BookingReview.objects.create(
+            author=self.user,
+            email="test@email.com",
+            comment="The booking was good",
+            service_rating=4,
+            food_rating=3,
+            overall_rating=4,
+        )
+
+    def test_post_review_view(self):
+        """
+        Logs in user to check if they can access the review form
+        """
+        self.client.login(
+            username="testuser",
+            password="testpassword"
+        )
+
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response='reviews/index.html')
+
